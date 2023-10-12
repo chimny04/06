68304aea9657ddc6cab9a1ef6a176d9080c87d59
chimny04 <lucia.roback@gmail.com>
2023-10-12 16:57:38 +0900

    6-5

--- main.c
+++ main.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
+int factorial(int n)
 {
-    return (a*a);
+    int res = 1;
+    int i;
+    
+    for(i=1; i<=n; i++)
+        res *= i;
+    
+    return res;
+}
+
+int combination(int n, int r)
+{
+    int div1, div2; // div1 = 분자, div2 = 분모
+    
+    div1 = factorial(n); // n!
+    div2 = factorial(n-r) * factorial(r);
+    
+    return (div1/div2);
 }
 
 int main()
 {
-    int a = 2;
-    a = square(a);
-    printf("a = %i\n", a);
-  
+    int n, r;
+    int div1, div2; // div1 = 분자, div2 = 분모
+    int result;
+    
+    // 입력값 받음 
+    printf("input n: ");
+    scanf("%d", &n);
+    printf("input r: ");
+    scanf("%d", &r);
+    
+    // combination 계산 
+    result = combination(n,r);
+    
+    // 결과 값 출력 
+    printf("result is %i\n", result);
+    
     system("PAUSE");	
     return 0;
 }
